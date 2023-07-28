ed40784443f9db9d315b9af2b197c691a2dc51ba
Jie Weng <jerrywossion@gmail.com>
2023-07-28 10:02:50 +0800

    add prettier

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '西風之眠';
-export const SITE_DESCRIPTION = '涼風起天末 君子意如何';
+export const SITE_TITLE = '西風之眠'
+export const SITE_DESCRIPTION = '涼風起天末 君子意如何'
